eb66e7f10427118fc3ee8c51a2f60d5b4e8353ee
phil-schmidtke <mail@phil-schmidtke.de>
2022-11-24 20:38:03 +0100

    parse env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+require('dotenv').config({ path: '.env'});
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    OPENAI: process.env.OPENAI,
+  }
 }
 
 module.exports = nextConfig
