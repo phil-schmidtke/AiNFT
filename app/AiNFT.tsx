42ad2a7576f259f404f4f5bae61ea60decd674ee
phil-schmidtke <mail@phil-schmidtke.de>
2022-11-24 19:43:51 +0100

    Spline and Design

---
+++ app/AiNFT.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Spline from '@splinetool/react-spline';
+
+export default function AiNFT() {
+  return (
+    <div className=''> 
+        <Spline scene="https://prod.spline.design/dGxcJCS2wadtJWfV/scene.splinecode" height={500} />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-
+import AiNFT from "./AiNFT"
 
 export default function Home() {
   return (
-    <div>
-      AiNft
+    <div className='w-full h-screen'>
+      <AiNFT />
+      <div className="flex">
+        <button className="text-xl mx-auto p-4 bg-fuchsia-500 hover:bg-fuchsia-600 rounded-xl shadow-2xl">Get yours now!</button>
+      </div>
     </div>
   )
 }
